f4df589ecb420faea74bd5e742495bf33218ea1c
Axel Ekwall <axel@minimize.se>
2019-10-20 22:54:45 +0200

    add helper to concat classes

--- src/atoms/Link.tsx
+++ src/atoms/Link.tsx
@@ -1,34 +1,46 @@
 import React, { FunctionComponent } from 'react';
 import { Link as GatsbyLink, GatsbyLinkProps } from 'gatsby';
+import { combineClasses } from '../utils/helpers';
 
 const Link: FunctionComponent<GatsbyLinkProps<{}>> = ({
   children,
   to,
   activeClassName,
   partiallyActive,
+  className,
   ref,
   ...other
 }) => {
   const internal = /^\/(?!\/)/.test(to);
   const file = /\.[0-9a-z]+$/i.test(to);
 
+  const combinedClassName = combineClasses([
+    'font-sans text-base text-sincere-green',
+    className,
+  ]);
+
   if (internal) {
     if (file) {
       return (
-        <a href={to} ref={ref} {...other}>
+        <a className={combinedClassName} href={to} ref={ref} {...other}>
           {children}
         </a>
       );
     } else {
       return (
-        <GatsbyLink to={to} activeClassName={activeClassName} {...other}>
+        <GatsbyLink
+          className={combinedClassName}
+          to={to}
+          activeClassName={activeClassName}
+          {...other}
+        >
           {children}
         </GatsbyLink>
       );
     }
   } else {
     return (
-      <a href={to} ref={ref} {...other}>
+      <a className={combinedClassName} href={to} ref={ref} {...other}>
         {children}
       </a>
     );
--- src/organisms/Layout.tsx
+++ src/organisms/Layout.tsx
@@ -1,14 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import { Helmet } from 'react-helmet';
 import Footer from './Footer';
+import { combineClasses } from '../utils/helpers';
 
 interface Props {
   title: string;
+  className?: string;
 }
 
-const Layout: FunctionComponent<Props> = ({ title, children }) => {
+const Layout: FunctionComponent<Props> = ({ title, children, className }) => {
   return (
-    <div className="min-h-screen flex flex-col w-full bg-sincere-background">
+    <div
+      className={combineClasses([
+        'min-h-screen flex flex-col w-full bg-sincere-background',
+        className,
+      ])}
+    >
       <Helmet>
         <meta charSet="utf-8" />
         <title>{title}</title>
