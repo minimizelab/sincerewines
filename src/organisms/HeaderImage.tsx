9f5a855062f6042688abc43501b33ea7532f2c54
Axel Ekwall <git@axelekwall.se>
2019-10-29 19:16:30 +0100

    add gatsby-image

--- src/organisms/HeaderImage.tsx
+++ src/organisms/HeaderImage.tsx
@@ -2,13 +2,32 @@ import React, { FunctionComponent } from 'react';
 import H4 from '../atoms/H4';
 import Text from '../atoms/Text';
 import Button from '../molecules/Button';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-const headerImg = require('../assets/headerimg.png');
+// const headerImg = require('../assets/headerimg.png');
 
 const HeaderImage: FunctionComponent = () => (
   <div className="flex flex-col justify-end items-start">
-    <img className="w-screen" src={headerImg} />
-
+    <StaticQuery
+      query={graphql`
+        query {
+          file(
+            relativePath: { eq: "headerimg.png" }
+            sourceInstanceName: { eq: "images" }
+          ) {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <Img className="w-full" fluid={data.file.childImageSharp.fluid}></Img>
+      )}
+    />
     <div className="py-20 px-16 absolute">
       <H4 white className="text-white">
         A Swedish Premium Wine Importer
