9482225c0c845bcf9b83099cefe1885b49eafd3f
Ella <ella.wg@hotmail.com>
2019-11-03 17:46:50 +0100

    add sortiment again

--- src/pages/sortiment.tsx
+++ src/pages/sortiment.tsx
@@ -1,29 +1,18 @@
 import React, { FunctionComponent } from 'react';
 import Layout from '../organisms/Layout';
+import ProductCardList from '../organisms/ProductCardList';
 import Section from '../atoms/Section';
 import H1 from '../atoms/H1';
-import TextLarge from '../atoms/TextLarge';
-import ArrowLink from '../atoms/ArrowLink';
 
-const Bestallningar: FunctionComponent = () => (
-  <Layout title="Beställningar">
-    <Section className="justify-center">
-      <div className="w-2/5 flex flex-col pt-3 mt-12 mb-6">
-        <H1>Beställningar</H1>
-        <TextLarge>
-          Snart kommer mer information om hur du beställer våra viner från
-          systembolaget här.
-        </TextLarge>
-        <TextLarge className="my-4">
-          Kontakta oss, eller besök systembolagets webbplats för mer
-          information.
-        </TextLarge>
-        <ArrowLink to="https://www.systembolaget.se/bestalla-drycker/">
-          beställa drycker via systembolaget
-        </ArrowLink>
-      </div>
+const Sortiment: FunctionComponent = () => (
+  <Layout title="Sortiment">
+    <Section className="flex-row justify-center p-6 mt-12 mb-6">
+      <H1>Våra Viner</H1>
+    </Section>
+    <Section className="mb-6">
+      <ProductCardList />
     </Section>
   </Layout>
 );
 
-export default Bestallningar;
+export default Sortiment;
