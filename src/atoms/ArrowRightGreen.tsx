838804744c53e366ac3f689d81a3599f0843513d
Ella <ella.wg@hotmail.com>
2019-11-03 11:33:36 +0100

    add arrow

---
+++ src/atoms/ArrowRightGreen.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const arrowIcon = require('../assets/arrow-right-green.png');
+
+const ArrowRightGreen = () => <img className="w-12" src={arrowIcon} />;
+
+export default ArrowRightGreen;
--- src/organisms/DetailedLinks.tsx
+++ src/organisms/DetailedLinks.tsx
@@ -1,27 +1,31 @@
 import React, { FunctionComponent } from 'react';
 import Block from '../molecules/Block';
 import Text from '../atoms/Text';
+import ArrowRightGreen from '../atoms/ArrowRightGreen';
 
 const DetailedLinks: FunctionComponent = () => (
   <div className="flex flex-row w-full items-end justify-between m-6 p-6 flex-wrap bg-white -mt-8 z-30">
     <div className="flex flex-row items-start lg:w-auto w-full flex-wrap">
-      <Block center title="Utforska">
+      <Block center title="Arbetet på gården">
         <Text>
           Läs mer om hur våra producenter arbetar för att leverera viner av
           högsta kvalitet
         </Text>
+        <ArrowRightGreen />
       </Block>
-      <Block center title="Lista">
+      <Block center title="Utvalda viner">
         <Text>
           Välj bland vårt importsortiment av viner och lista dina
           favoriter för smidig beställning
         </Text>
+        <ArrowRightGreen />
       </Block>
-      <Block center title="Beställ">
+      <Block center title="Beställ hem">
         <Text>
           Så här går det till när du enkelt beställer hem dina viner genom
           Systembolaget
         </Text>
+        <ArrowRightGreen />
       </Block>
     </div>
   </div>
