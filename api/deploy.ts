c03e5bea61571b89211276931f329a75635f8a40
Axel Ekwall <git@axelekwall.se>
2020-03-31 12:27:15 +0200

    WIP

--- api/deploy.ts
+++ api/deploy.ts
@@ -1,9 +1,25 @@
 import { NowRequest, NowResponse } from '@now/node';
+import got from 'got';
+
+const mini_teamId = 'TEAM ID HERE';
+
+const nowApi = got.extend({
+  prefixUrl: 'https://api.zeit.co',
+  headers: {
+    Authorization: `Bearer ${process.env.NOW_TOKEN}`,
+    'Content-Type': 'application/json',
+  },
+});
 
 type Deploy = (req: NowRequest, res: NowResponse) => void;
 
-const deploy: Deploy = (req, res) => {
-  res.send('hello from api');
+const deploy: Deploy = async (req, res) => {
+  try {
+    const data = await nowApi(`/v5/now/deployments?teamId=${mini_teamId}`);
+    res.status(200).send(JSON.parse(data.body));
+  } catch (error) {
+    res.status(503).send(`Error: ${error}`);
+  }
 };
 
 export default deploy;
