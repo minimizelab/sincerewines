75f9128f2d0e72cdfd51c78ec0eec1089c2de50a
Ella <ella.wg@hotmail.com>
2020-05-10 21:03:35 +0200

    Add arrow link

--- www/src/molecules/WineListCard.tsx
+++ www/src/molecules/WineListCard.tsx
@@ -8,12 +8,12 @@ import { Wine } from '../types/types';
 import H5 from '../atoms/H5';
 import Text from '../atoms/Text';
 import { Image, useSanityImage } from '@minimizelab/mini_ui-react';
-import { navigate } from 'gatsby';
 import TypeIndicator from '../atoms/TypeIndicator';
 import { createArrayString } from '../utils/functions';
 import QuantityButton from '../atoms/QuantityButton';
 import ListIndicator from '../atoms/ListIndicator';
 import WineListDetails from './WineListDetails';
+import ArrowLink from '../atoms/ArrowLink';
 
 interface Props {
   item: Wine;
@@ -44,25 +44,17 @@ const WineListCard: FunctionComponent<Props> = ({ item }) => {
     dispatch(actions.decreaseQuantity(item.id));
   };
 
-  const handleOnClick = (event: any): void => {
-    event.preventDefault();
-    navigate(`/sortiment/${item.path.current}`);
-  };
-
   return (
     <div className="w-full relative sm:w-2/3">
-      <div
-        onClick={handleOnClick}
-        className="bg-white h-208 rounded shadow mx-6 my-3 md:my-6 p-6 flex flex-row cursor-pointer"
-      >
+      <div className="bg-white h-270 lg:h-208 rounded shadow mx-6 my-3 md:my-6 p-6 flex flex-row">
         <div className="flex flex-col w-16 justify-center items-center">
           <Image
             {...imageProps}
             aspectRatio={item.image.asset.metadata.dimensions.aspectRatio}
           />
         </div>
-        <div className="flex flex-row lg:flex-grow flex-wrap sm:pr-16">
-          <div className="flex flex-col p-2 items-start flex-grow justify-around">
+        <div className="flex flex-row flex-wrap w-full pr-10 md:pr-16">
+          <div className="flex flex-col p-2 items-start lg:flex-grow justify-around">
             <Text>{item.producer.name}</Text>
             <H5>{item.name}</H5>
             <H5>{item.year}</H5>
@@ -70,25 +62,33 @@ const WineListCard: FunctionComponent<Props> = ({ item }) => {
               {createArrayString(item.grapes.map((item) => item.name))}
             </Text>
           </div>
-          <div className="flex flex-row lg:w-1/3 md:w-3/4 sm:w-full select-none">
-            <WineListDetails title="Antal">
-              <div className="flex flex-row justify-between">
-                <p>{itemQuantity()}</p>
-                <QuantityButton
-                  onIncrease={increaseQuantity}
-                  onDecrease={decreaseQuantity}
-                />
-              </div>
-            </WineListDetails>
-            <WineListDetails title="Volym">
-              <p>{item.vol * itemQuantity() + ' cl'}</p>
-            </WineListDetails>
-            <WineListDetails title="Pris">
-              <p className="whitespace-no-wrap">
-                {item.price * itemQuantity() + ' kr'}
-              </p>
-            </WineListDetails>
+          <div className="flex flex-col justify-between lg:w-1/3 w-full">
+            <div className="flex flex-row w-full select-none p-2">
+              <WineListDetails title="Antal">
+                <div className="flex flex-row justify-between">
+                  <p>{itemQuantity()}</p>
+                  <QuantityButton
+                    onIncrease={increaseQuantity}
+                    onDecrease={decreaseQuantity}
+                  />
+                </div>
+              </WineListDetails>
+              <WineListDetails title="Volym">
+                <p>{item.vol * itemQuantity() + ' cl'}</p>
+              </WineListDetails>
+              <WineListDetails title="Pris">
+                <p className="whitespace-no-wrap">
+                  {item.price * itemQuantity() + ' kr'}
+                </p>
+              </WineListDetails>
+            </div>
+            <div className="self-end">
+              <ArrowLink to={`/sortiment/${item.path.current}`}>
+                Läs mer om vinet
+              </ArrowLink>
+            </div>
           </div>
+
           <ListIndicator
             className="absolute m-10 top-0 right-0"
             inList={true}
--- www/src/pages/vinlista.tsx
+++ www/src/pages/vinlista.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { State } from '../store';
 import Layout from '../organisms/Layout';
@@ -7,6 +7,7 @@ import H1 from '../atoms/H1';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Wine, WineCase } from '../types/types';
 import ProductCardList from '../organisms/ProductCardList';
+import H3 from '../atoms/H3';
 
 const Vinlista: FunctionComponent = () => {
   const wineList = useSelector<State, { id: string; quantity: number }[]>(
@@ -57,18 +58,37 @@ const Vinlista: FunctionComponent = () => {
       }
     }
   `);
+  // const wineData = () =>
+  //   allSanityWine.edges.filter((item) =>
+  //     wineList.map((wine) => wine.id).includes(item.node.id)
+  //   );
+  const wineData = useMemo(() => {
+    const wineListIds = wineList.map((wine) => wine.id);
+    const wine = allSanityWine.edges.filter(({ node }) =>
+      wineListIds.includes(node.id)
+    );
+    return wine;
+  }, [wineList]);
+  const totalPrice = useMemo(() => {
+    let total = 0;
+    wineList.forEach((wine) => {
+      const price = wineData
+        .filter(({ node }) => node.id === wine.id)
+        .map((edge) => edge.node)[0].price;
+      total += wine.quantity * price;
+    });
+    return total;
+  }, [wineList, wineData]);
   return (
     <Layout title="Vinlista" description="Din vinlista">
       <Section className="flex-row justify-center pt-3 mt-12">
         <H1>Vinlista</H1>
       </Section>
       <Section className="my-6 flex-col">
-        <ProductCardList
-          wineList
-          data={allSanityWine.edges.filter((item) =>
-            wineList.map((wine) => wine.id).includes(item.node.id)
-          )}
-        />
+        <ProductCardList wineList data={wineData} />
+      </Section>
+      <Section className="flex-row justify-center pt-3 mb-12">
+        <H3>Totalt: {totalPrice} kr</H3>
       </Section>
     </Layout>
   );
