ff4ef5a124fbc52cfe935c568119102b0a7aa37a
Axel Ekwall <axel.ekwall@hey.com>
2021-01-24 19:23:59 +0100

    feat: fix admin access

--- www/next.config.js
+++ www/next.config.js
@@ -1,5 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/admin/:slug',
+        destination: 'https://sw-admin.minimize.se/admin/:slug',
+      },
+    ];
   },
 };
