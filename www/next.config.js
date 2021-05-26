ed66ea03007b0eae168ed4f1886b8c70ee85785e
Axel Ekwall <axel@minimize.se>
2021-05-26 17:02:02 +0200

    feat: /admin route redirect to admin UI (#49)
    * feat: fix admin access
    
    * fix: revert last fix
    
    * fix: redirects update

--- www/next.config.js
+++ www/next.config.js
@@ -1,6 +1,16 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/admin/:p*',
+        destination: 'https://sw-admin.minimize.se/',
+        permanent: true,
+      },
+    ];
   },
 };
