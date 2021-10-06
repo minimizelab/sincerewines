ababf172f79802619f8a820bc67aa211834950e5
Axel Ekwall <git@axelekwall.se>
2021-10-06 15:40:59 +0200

    upgrade packages and config

--- www/.eslintrc.js
+++ www/.eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'plugin:prettier/recommended',
   ],
   settings: {
